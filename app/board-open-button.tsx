6460b0eb2bfd56af50c1bebf6f561076048bed7e
nyu1791-collab <n.yu1791@gmail.com>
2026-09-15 16:55:01 +0900

    Remove obsolete reconstructed ranking navigation

--- app/board-open-button.tsx
+++
@@ -1,4 +0,0 @@
-'use client';
-export default function BoardOpenButton({className,children}:{className?:string;children:React.ReactNode}){
- return <button type="button" className={className} onClick={()=>{window.location.assign('/boards');}}>{children}</button>;
-}
